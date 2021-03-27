19301bf768b02d11987e254a0fab8adc4ae1a64c
Barogthor <f.thery3103@gmail.com>
2021-03-28 00:28:09 +0100

    glium black screen render

---
+++ helper/src/lib.rs
@@ -0,0 +1,82 @@
+
+use rand::Rng;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Clone, PartialOrd, PartialEq, Debug)]
+pub enum Colors {
+    RED,
+    BLUE,
+    GREEN,
+    MAGENTA,
+    YELLOW,
+    TEAL,
+    WHITE,
+    GREY,
+    BLACK,
+    Other(u8, u8, u8, u8)
+}
+
+impl Colors {
+    pub fn random() -> Self {
+        let mut rng = rand::thread_rng();
+        let n = rng.gen::<u32>();
+        match n % 9 {
+            0 => Colors::RED,
+            1 => Colors::BLUE,
+            2 => Colors::GREEN,
+            3 => Colors::MAGENTA,
+            4 => Colors::YELLOW,
+            5 => Colors::TEAL,
+            6 => Colors::WHITE,
+            7 => Colors::GREY,
+            8 => Colors::BLACK,
+            _ => panic!("Unexpected random for color conversion"),
+        }
+    }
+
+}
+
+impl From<Colors> for (f32, f32,f32) {
+    fn from(color: Colors) -> Self {
+        match color {
+            Colors::RED     => (1., 0., 0.),
+            Colors::GREEN   => (0., 1., 0.),
+            Colors::BLUE    => (0., 0., 1.),
+            Colors::YELLOW  => (1., 1., 0.),
+            Colors::MAGENTA => (1., 0., 1.),
+            Colors::TEAL    => (0., 1., 1.),
+            Colors::GREY    => (0.5, 0.5, 0.5),
+            Colors::BLACK   => (0., 0., 0.),
+            Colors::WHITE   => (1., 1., 1.),
+            Colors::Other(r, g, b, _a) => { (r as f32/ 255., g as f32 / 255., b as f32 / 255.) }
+        }
+    }
+}
+impl From<Colors> for (f32, f32, f32, f32) {
+    fn from(color: Colors) -> Self {
+        match color {
+            Colors::RED     => (1., 0., 0., 1.),
+            Colors::GREEN   => (0., 1., 0., 1.),
+            Colors::BLUE    => (0., 0., 1., 1.),
+            Colors::YELLOW  => (1., 1., 0., 1.),
+            Colors::MAGENTA => (1., 0., 1., 1.),
+            Colors::TEAL    => (0., 1., 1., 1.),
+            Colors::GREY    => (0.5, 0.5, 0.5, 1.),
+            Colors::BLACK   => (0., 0., 0., 1.),
+            Colors::WHITE   => (1., 1., 1., 1.),
+            Colors::Other(r, g, b, _a) => { (r as f32 / 255., g as f32 / 255., b as f32 / 255., 1.) }
+        }
+    }
+}
+
+pub fn load_glsl(path: &str) -> String {
+    let mut nice_shader = String::new();
+    File::open(path)
+        .unwrap()
+        .read_to_string(&mut nice_shader)
+        .unwrap();
+    nice_shader
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use glium::{Display, glutin, Surface};
+use winit::event::{Event, StartCause};
+use winit::window::Fullscreen;
+
+use helper::Colors;
+
+pub type LoopType = glium::glutin::event_loop::EventLoop<()>;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop: LoopType = LoopType::new();
+    let wb = glutin::window::WindowBuilder::new().with_title("3D Playground");
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::NewEvents(cause) => match cause {
+            StartCause::ResumeTimeReached { .. } => {}
+            StartCause::WaitCancelled { .. } => {}
+            StartCause::Poll => {}
+            StartCause::Init => {}
+        },
+        Event::MainEventsCleared => {
+            display.gl_window().window().request_redraw();
+        }
+        Event::RedrawRequested(_) => {
+            let mut frame = display.draw();
+            frame.clear_color_and_depth(Colors::RED.into(), 1.);
+            frame.finish().unwrap()
+        }
+        Event::RedrawEventsCleared => {}
+        _ => {},
+    });
+}
+
+fn set_fullscreen(display: &Display, fullscreen: &mut bool) {
+    if *fullscreen {
+        display.gl_window().window().set_fullscreen(None);
+        *fullscreen = false;
+    } else {
+        let monitor_handle = display
+            .gl_window()
+            .window()
+            .available_monitors()
+            .next()
+            .unwrap();
+        let fs = Fullscreen::Borderless(Some(monitor_handle));
+        display.gl_window().window().set_fullscreen(Some(fs));
+
+        *fullscreen = true;
+    }
 }
