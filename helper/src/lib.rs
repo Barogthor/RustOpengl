7e516d622825594148312f3d704c5f4e1aadc8db
Barogthor <f.thery3103@gmail.com>
2021-03-28 17:40:33 +0200

    refactor colors

--- helper/src/lib.rs
+++ helper/src/lib.rs
@@ -34,40 +34,43 @@ impl Colors {
         }
     }
 
+    fn to_tuple(self) -> (u8, u8, u8, u8) {
+        match self {
+            Colors::RED => (255, 0, 0, 255),
+            Colors::GREEN => (0, 255, 0, 255),
+            Colors::BLUE => (0, 0, 255, 255),
+            Colors::YELLOW => (255, 255, 0, 255),
+            Colors::MAGENTA => (255, 0, 255, 255),
+            Colors::TEAL => (0, 255, 255, 255),
+            Colors::GREY => (128, 128, 128, 255),
+            Colors::BLACK => (0, 0, 0, 255),
+            Colors::WHITE => (255, 255, 255, 255),
+            Colors::Other(r, g, b, a) => { (r, g, b, a) }
+        }
+    }
+
+
 }
 
 impl From<Colors> for (f32, f32,f32) {
     fn from(color: Colors) -> Self {
-        match color {
-            Colors::RED     => (1., 0., 0.),
-            Colors::GREEN   => (0., 1., 0.),
-            Colors::BLUE    => (0., 0., 1.),
-            Colors::YELLOW  => (1., 1., 0.),
-            Colors::MAGENTA => (1., 0., 1.),
-            Colors::TEAL    => (0., 1., 1.),
-            Colors::GREY    => (0.5, 0.5, 0.5),
-            Colors::BLACK   => (0., 0., 0.),
-            Colors::WHITE   => (1., 1., 1.),
-            Colors::Other(r, g, b, _a) => { (r as f32/ 255., g as f32 / 255., b as f32 / 255.) }
-        }
+        let color = color.to_tuple();
+        (color.0 as f32 / 255., color.1 as f32 / 255., color.2 as f32 / 255.)
     }
 }
 impl From<Colors> for (f32, f32, f32, f32) {
     fn from(color: Colors) -> Self {
-        match color {
-            Colors::RED     => (1., 0., 0., 1.),
-            Colors::GREEN   => (0., 1., 0., 1.),
-            Colors::BLUE    => (0., 0., 1., 1.),
-            Colors::YELLOW  => (1., 1., 0., 1.),
-            Colors::MAGENTA => (1., 0., 1., 1.),
-            Colors::TEAL    => (0., 1., 1., 1.),
-            Colors::GREY    => (0.5, 0.5, 0.5, 1.),
-            Colors::BLACK   => (0., 0., 0., 1.),
-            Colors::WHITE   => (1., 1., 1., 1.),
-            Colors::Other(r, g, b, _a) => { (r as f32 / 255., g as f32 / 255., b as f32 / 255., 1.) }
-        }
+        let color = color.to_tuple();
+        (color.0 as f32 / 255., color.1 as f32 / 255., color.2 as f32 / 255., color.3 as f32 / 255.)
     }
 }
+impl From<Colors> for [f32; 3] {
+    fn from(color: Colors) -> Self {
+        let color = color.to_tuple();
+        [color.0 as f32 / 255., color.1 as f32 / 255., color.2 as f32 / 255.]
+    }
+}
+
 
 pub fn load_glsl(path: &str) -> String {
     let mut nice_shader = String::new();
