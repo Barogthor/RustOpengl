8aab8a0639581607f15e9d00e0f11582309f1f4f
Barogthor <f.thery3103@gmail.com>
2021-04-03 01:00:12 +0200

    normal matrix

--- resources/shaders/basic_lighting.vs.glsl
+++ resources/shaders/basic_lighting.vs.glsl
@@ -12,5 +12,5 @@ uniform mat4 model;
 void main() {
     gl_Position = vp * model * vec4(position, 1.0);
     fragPos = vec3(model * vec4(position, 1.0));
-    oNormal = normal;
+    oNormal = mat3(transpose(inverse(model))) * normal;// normal matrix costly, should calculate on cpu and use uniform
 }
